bebe58acc4580283c8314a8bd528b2be3cf47cc0
afshin-x76 <afshin.kh.dev76@gmail.com>
2021-03-30 08:37:14 +0100

    change database to postgresql

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ShopProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shopdb',
+        'USER': os.environ.get('MY_DATABASE_USER'),
+        'PASSWORD': os.environ.get('MY_DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
