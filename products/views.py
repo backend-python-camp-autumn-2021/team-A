aa59e7cde02e58f4dd8dda314187b4e9388f5e31
afshin-x76 <afshin.kh.dev76@gmail.com>
2021-05-02 15:33:28 +0100

    Upgrade Group class

--- products/views.py
+++ products/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from users.models import User, Supplier, Customer
-from .models import Category, Tag, Brand
+from .models import Category, Tag, Brand, Product
+from django.db.models import Q
 
 
 class Gruoping(View):
     '''
-    we literally need the some grouping in many pages.
-    if you want your own custome gruoing you can custom handele_context method.
+    you can inherit this class if you want to have grouping
+    and filter and search functionality in your view.
+    add this line in your get method.
+    <context_name> = self.get_query_set(request, query_set=None)
     '''
+
+    queryset_name = 'query_set'
+    
     def handle_context(self, context=None):
         '''
         add tags and categories and brands to the given context
@@ -20,7 +26,30 @@ class Gruoping(View):
         context['brands'] = Brand.objects.all()
         context['tags'] = Tag.objects.all()[:30]
         return context
-
+    
+    def get_query_set(self, request, query_set=None):
+        '''
+        filter queryset base on given query.
+        filter is base on this keywords --> brand, cat, tag, q
+        q will query on name and description fileds.
+        return all products if there was'nt any query.
+        '''
+        if not query_set:
+            query_set = Product.objects.all()
+        if 'cat' in request.GET:
+            query_set = query_set.filter(category__pk=request.GET.get('cat'))
+        elif 'brand' in request.GET:
+            query_set = query_set.filter(brand__pk=request.GET.get('brand'))
+        elif 'tag' in request.GET:
+            query_set = query_set.filter(tag__pk=request.GET.get('tag'))
+        elif 'q' in request.GET:
+            query_set = query_set.filter(
+                Q(name__icontains=request.GET.get('q'))|
+                Q(description__icontains=request.GET.get('q'))
+                )
+        context = self.handle_context()
+        context[self.queryset_name] = query_set
+        return context
  
 
 
