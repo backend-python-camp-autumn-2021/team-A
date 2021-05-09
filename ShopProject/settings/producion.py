86fde59ea0566012d0387f32ea3e484994b2fbc3
afshin-x76 <afshin@afshin.com>
2021-05-09 14:31:47 +0100

    change settings

--- ShopProject/settings/producion.py
+++ ShopProject/settings/producion.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_DEV_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
