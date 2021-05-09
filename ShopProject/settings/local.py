51a8afaa9c7c9c969c54ef9e69cc787819eb981b
afshin-x76 <afshin@afshin.com>
2021-05-09 14:31:47 +0100

    change settings

--- ShopProject/settings/local.py
+++ ShopProject/settings/local.py
@@ -2,6 +2,40 @@ from .base import *
 
 DEBUG = True
 
+INSTALLED_APPS += ['debug_toolbar',]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_DEV_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'static_env'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
 
 # LOGGING = {
 #     'version': 1,
@@ -21,26 +55,3 @@ DEBUG = True
 #         },
 #     },
 # }
-
-INSTALLED_APPS += ['debug_toolbar',]
-
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
-
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
-STATIC_ROOT = BASE_DIR / 'static_env'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_URL = '/static/'
-
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'afshin.kh.dev76@gmail.com'
-EMAIL_HOST_PASSWORD = '5334570396'
